90ba82aaabfa226a1642d0fb133e576567b46e1c
tony84727 <tony84727@gmail.com>
2019-07-10 13:32:10 +0800

    add glium/window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate glium;
+
 fn main() {
-    println!("Hello, world!");
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let wb = glium::glutin::WindowBuilder::new()
+        .with_dimensions(glium::glutin::dpi::LogicalSize{
+            width: 1024_f64,
+            height: 768_f64,
+        })
+        .with_title("OpenWorld");
+    let cb = glium::glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &events_loop).unwrap();
 }
