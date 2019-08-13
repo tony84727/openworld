bab5c0951737474fc6942d5a65913de022faa747
tony84727 <tony84727@gmail.com>
2019-08-13 22:44:04 +0800

    use internal control system for walking. update Cargo.toml.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use amethyst::{
     assets::{PrefabLoader, PrefabLoaderSystemDesc, RonFormat},
+    controls::{FlyControlBundle, FlyControlTag},
     core::transform::{Transform, TransformBundle},
     input::{InputBundle, StringBindings},
     prelude::*,
@@ -10,8 +11,7 @@ use amethyst::{
     },
     utils::{application_root_dir, scene::BasicScenePrefab},
 };
-mod component;
-mod input;
+
 struct InWorld;
 
 impl SimpleState for InWorld {
@@ -20,8 +20,9 @@ impl SimpleState for InWorld {
         let camera = Camera::standard_3d(1024.0, 768.0);
         world
             .create_entity()
+            .named("Camera")
             .with(Transform::default())
-            .with(component::Player::new())
+            .with(FlyControlTag)
             .with(camera)
             .build();
         let handle = world.exec(|loader: PrefabLoader<'_, ScenePrefabData>| {
@@ -39,12 +40,10 @@ fn main() -> amethyst::Result<()> {
     let config_dir = app_root.join("config");
     let asset_dir = app_root.join("assets");
     let game_data = GameDataBuilder::default()
-        .with_bundle(TransformBundle::new())?
         .with_bundle(
             InputBundle::<StringBindings>::new()
                 .with_bindings_from_file(config_dir.join("input.ron"))?,
         )?
-        .with(input::MovementSystem, "", &[])
         .with_system_desc(
             PrefabLoaderSystemDesc::<ScenePrefabData>::default(),
             "",
@@ -58,7 +57,17 @@ fn main() -> amethyst::Result<()> {
                 )
                 .with_plugin(RenderSkybox::default())
                 .with_plugin(RenderFlat3D::default()),
-        )?;
+        )?
+        .with_bundle(
+            FlyControlBundle::<StringBindings>::new(
+                Some(String::from("horizontal")),
+                None,
+                Some(String::from("vertical")),
+            )
+            .with_sensitivity(0.1, 0.1)
+            .with_speed(4.0),
+        )?
+        .with_bundle(TransformBundle::new().with_dep(&["fly_movement"]))?;
     let mut game = Application::new(asset_dir, InWorld, game_data)?;
     game.run();
     Ok(())
